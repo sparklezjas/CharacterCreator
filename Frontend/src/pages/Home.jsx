964a63b8fa1266847aef4e8d8f4c9f43dd532c4d
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-07-22 12:34:14 -0500

    Fixed sorting feature and some css on home

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,76 +4,73 @@ import axios from 'axios'
 import Character from "../components/Character"
 
 const Home = () => {
-    const {user} = useAuthContext()
-    const [allCharacters, setAllCharacters] = useState([]);
-    const [sortOrder, setSortOrder] = useState();
-    
+    const { user } = useAuthContext()
+    const [allCharacters, setAllCharacters] = useState([])
+    const [sortedCharacters, setSortedCharacters] = useState([])
+    const [sortOrder, setSortOrder] = useState()
 
     useEffect(() => {
-        axios
-            .get('http://localhost:4000/api/characters/all')
-            .then((res) => {
-                console.log(res);
-                setAllCharacters(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, []);
+    axios
+        .get('http://localhost:4000/api/characters/all')
+        .then((res) => {
+        console.log(res)
+        setAllCharacters(res.data)
+        setSortedCharacters(res.data)
+        })
+        .catch((err) => {
+        console.log(err)
+    })
+}, [])
 
     useEffect(() => {
-        const characterOrder = [...allCharacters].sort((first, last) => {
-            const oldest = new Date(first.createdAt);
-            const newest = new Date(last.createdAt);
-            if (sortOrder === 'oldest') {
-                return oldest - newest
-            }  else {
-                return newest - oldest;
-            }
-        });
-        setAllCharacters(characterOrder);
-    }, [sortOrder]);
+    if (sortOrder === 'oldest') {
+        setSortedCharacters([...allCharacters].sort((first, last) => {
+        const oldest = new Date(first.createdAt)
+        const newest = new Date(last.createdAt)
+        return oldest - newest
+    }))
+    } else {
+        setSortedCharacters([...allCharacters].sort((first, last) => {
+        const oldest = new Date(first.createdAt)
+        const newest = new Date(last.createdAt)
+        return newest - oldest
+    }))
+    }
+}, [sortOrder, allCharacters])
 
     const sortByOldest = () => {
-        setSortOrder('oldest')
-    }
+    setSortOrder('oldest')
+}
 
     const sortByNewest = () => {
-        setSortOrder('newest')
+    setSortOrder('newest')
     }
 
     const deleteHandler = (id) => {
-        axios
-          .delete(`http://localhost:4000/api/characters/delete/${id}`)
-          .then((res) => {
+    axios
+            .delete(`http://localhost:4000/api/characters/delete/${id}`)
+            .then((res) => {
+                setAllCharacters(allCharacters.filter(character => character._id !== id))
+                setSortedCharacters(sortedCharacters.filter(character => character._id !== id))
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
 
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      };
-
-      useEffect(() => {
-        axios.get(`http://localhost:4000/api/characters/all`)
-        .then((res) => {
-            setAllCharacters(res.data)
-        })
-        .catch((err) => console.log(err))
-      }, [deleteHandler])
-return (
-    <div className="home">
-        <div className="characters">
-                <button onClick={sortByNewest}>Newest</button>
-                <button onClick={sortByOldest}>Oldest</button>
-                {allCharacters && allCharacters.map((character) => (
-                    <Character key={character._id} 
-                    character = {character}
-                    onDelete={() => deleteHandler(character._id)}
+    return (
+        <div className="home">
+            <button onClick={sortByNewest}>Newest</button>
+            <button onClick={sortByOldest}>Oldest</button>
+            <div className="characters-container" style={{ display: 'flex', flexWrap: 'wrap' }}>
+                {sortedCharacters && sortedCharacters.map((character) => (
+                    <Character key={character._id}
+                        character={character}
+                        onDelete={() => deleteHandler(character._id)}
                     />
-                    
                 ))}
+            </div>
         </div>
-    </div>
     )
 }
 
