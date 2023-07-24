cd3431bb3f8e9d83f29f264635b86fc683f822d3
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-07-23 20:45:49 -0500

    FULLY functioning crud with validators!

--- Backend/controllers/characterController.js
+++ Backend/controllers/characterController.js
@@ -1,3 +1,4 @@
+const validator = require('validator')
 const Characters = require(`../models/characterModel`)
 const mongoose = require ('mongoose')
 
@@ -22,6 +23,9 @@ const getCharacter = async (req, res) => {
 
 const createCharacter = async (req, res) => {
     const {head, face, body, arms, legs, weapon, characterName, HP} = req.body
+    if (!validator.isLength(characterName, { min: 1 })) {
+        return res.status(400).json({ error: 'Character name is required' });
+      }
     try{ 
         const character = await Characters.create({head, face, body, arms, legs, weapon, characterName, HP})
         res.status(200).json(character)
@@ -44,19 +48,33 @@ const deleteCharacter = async (req, res) => {
 }
 
 const updateCharacter = async (req, res) => {
-    const {id} = req.params
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such character'})
+    const { id } = req.params;
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such character' });
+      }
+    
+      const { characterName } = req.body;
+    
+      // Validate characterName using the same validations as in the model
+      if (!validator.isLength(characterName, { min: 1 })) {
+        return res.status(400).json({ error: 'Character name is required' });
+      }
+      if (!validator.isLength(characterName, { max: 20 })) {
+        return res.status(400).json({ error: 'Name must be 20 characters or less' });
+      }
+    
+      try {
+        const character = await Characters.findOneAndUpdate({ _id: id }, req.body, { new: true });
+        if (!character) {
+          return res.status(400).json({ error: 'No such character' });
+        }
+    
+        res.status(200).json(character);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
     }
-
-    const character = await Characters.findOneAndUpdate({_id: id}, {
-        ...req.body
-    })
-    if (!character){
-        return res.status(400).json({error: "No such character"})
-    }
-    res.status(200).json(character)
-}
 
 module.exports = {
     getCharacter,
