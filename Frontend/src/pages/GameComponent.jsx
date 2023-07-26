8754aee05f297fbc2bc1e7174375b25d5eb089f3
Luke Erickson <mom2seth09@hotmail.com>
2023-07-26 00:21:24 -0500

    updated graphics/logice for gameplay screens

--- Frontend/src/pages/GameComponent.jsx
+++ Frontend/src/pages/GameComponent.jsx
@@ -2,15 +2,23 @@ import React, { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import GameCharacter from "../components/GameCharacter"
 import { useParams, Link } from 'react-router-dom';
+import '../CSS/FightPages.css'
+import fightForest from '../images/fightForest.jpg'
+import bush from '../images/bush.png'
+import submit from '../images/buttons/submit.png'
+import retry from '../images/buttons/retry.png'
+
 
 const GameComponent = () => {
   const { id } = useParams();
-  const correctPhrase = "The young elf was in a forest and suddenly heard a rustle in the bushes."
   const [inputValue, setInputValue] = useState("")
   const [showPopup, setShowPopup] = useState(false)
   const [showTryAgain, setShowTryAgain] = useState(false)
   const [showGoToSecondPage, setShowGoToSecondPage] = useState(false)
   const [timeLeft, setTimeLeft] = useState(30)
+  
+  const correctPhrase = "The young champion was in a forest and suddenly heard a rustle in the bushes."
+  
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -63,44 +71,72 @@ const GameComponent = () => {
     navigate(`/typingtrials2/${id}`)
   }
 
+
+  // ------------------------------------------DISABLE TEXT SELECTION
+  const disableTextSelection = () => {
+    document.body.style.userSelect = 'none'
+  }
+
+  useEffect(() => {
+    disableTextSelection()
+    return () => {}}, [])
+  // ------------------------------------------DISABLE TEXT SELECTION
+
   return (
     <div>
-      
-      <h1>Type the following phrase within 30 seconds:</h1>
-      <p>{correctPhrase}</p>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-      />
-      <button onClick={handleCheckPhrase}>Submit</button>
-      
+      <div>
+        <img className='bush' src={bush} alt="bush"/>
+        <img className='fightBkgd' src={fightForest} alt="forest background" />
+      </div>
+      <div className="pull_up characterPosition">
+        <GameCharacter/>
+      </div>
+      <div className="fightInfo">
+        <h2 className="instructions">Type the following phrase:</h2>
+        <p className="typePhrase">{correctPhrase}</p>
+        <input
+          className="inputGameText"
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        /> <br />
+        <button
+        className="submitButton"
+        style={{backgroundImage:`url(${submit})`}}
+        onClick={handleCheckPhrase} 
+        type="submit"></button>
+      </div>
       {showPopup && (
         <div>
           {inputValue === correctPhrase ? (
             <>
-              <p>Great job!</p>
+              <p className="gameMessages greatJob">WELL DONE!</p>
               {showGoToSecondPage && (
-                <button onClick={() => handleGoToSecondPage(id)}>Inspect bushes</button>
+                <button
+                className="continueButton"
+                onClick={() => handleGoToSecondPage(id)}>Inspect the bushes...</button>
               )}
             </>
           ) : (
             <>
               {showTryAgain && (
-                <button onClick={handleReset}>Try again!</button>
+                <button
+                className="retryButton"
+                style={{backgroundImage:`url(${retry})`}}
+                onClick={handleReset}
+                type="button"></button>
               )}
             </>
           )}
         </div>
       )}
 
-      {timeLeft > 0 && !showPopup && <p>Time left: {timeLeft} seconds</p>}
-      <div className="pull_up">
-      <GameCharacter/>
-      </div>
+      {timeLeft > 0 && !showPopup && 
+      <p className="gameMessages">Time left...
+      <br />
+      <span className="seconds">{timeLeft} Sec</span></p>}
     </div>
-    
   );
 };
 
--- Frontend/src/pages/SecondPage.jsx
+++ Frontend/src/pages/SecondPage.jsx
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import GameCharacter from "../components/GameCharacter";
 import { useParams, Link } from 'react-router-dom';
+import '../CSS/FightPages.css'
+import fightForest from '../images/fightForest.jpg'
+import bush from '../images/bush.png'
+import submit from '../images/buttons/submit.png'
+import retry from '../images/buttons/retry.png'
+import enemy from "../images/enemies/skeleton-Idle_0.png"
 
 
 const SecondPage = () => {
   const { id } = useParams();
-  const correctPhrase = "The elf moved towards the bushes, and out popped a giant skeleton weaing a mario red shell looking thingy!"
+  const correctPhrase = "The champion moved towards the bushes and out popped a giant, mean, red crabby thingy!"
   const [inputValue, setInputValue] = useState("")
   const [showPopup, setShowPopup] = useState(false)
   const [showTryAgain, setShowTryAgain] = useState(false)
@@ -64,41 +70,71 @@ const SecondPage = () => {
     navigate(`/typingtrials3/${id}`)
   }
 
+// ------------------------------------------DISABLE TEXT SELECTION
+const disableTextSelection = () => {
+  document.body.style.userSelect = 'none'
+}
+
+useEffect(() => {
+  disableTextSelection()
+  return () => {}}, [])
+// ------------------------------------------DISABLE TEXT SELECTION
+
   return (
     <div>
-      <h1>Type the following phrase within 30 seconds:</h1>
-      <p>{correctPhrase}</p>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-      />
-      <button onClick={handleCheckPhrase}>Submit</button>
-
+      <div>
+        <img className='bush' src={bush} alt="bush"/>
+        <img src={enemy} alt="enemy" className="enemy enemyBush" />
+        <img className='fightBkgd' src={fightForest} alt="forest background" />
+      </div >
+      <div className="pull_up characterPosition">
+        <GameCharacter/>
+      </div>
+      <div className="fightInfo">
+      <h2 className="instructions">Type the following phrase:</h2>
+        <p className="typePhrase">{correctPhrase}</p>
+        <input
+          className="inputGameText"
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        /> <br />
+        <button 
+        className="submitButton"
+        style={{backgroundImage:`url(${submit})`}}
+        onClick={handleCheckPhrase}
+        type="submit"></button>
+      </div>
       {showPopup && (
         <div>
           {inputValue === correctPhrase ? (
             <>
-              <p>Great job!</p>
+              <p className="gameMessages greatJob">WELL DONE!</p>
               {showGoToSecondPage && (
-                <button onClick={() => handleGoToSecondPage(id)}>Fight!</button>
-              )}
+                <button 
+                className="continueButton"
+                onClick={() => handleGoToSecondPage(id)}>Fight!</button>
+                )}
             </>
           ) : (
             <>
               {showTryAgain && (
-                <button onClick={handleReset}>Try again!</button>
-              )}
+                <button 
+                className="retryButton"
+                style={{backgroundImage:`url(${retry})`}}
+                onClick={handleReset}
+                type="button"></button>
+                )}
             </>
           )}
         </div>
       )}
 
-      {timeLeft > 0 && !showPopup && <p>Time left: {timeLeft} seconds</p>}
-      <div className="pull_up">
-      <GameCharacter/>
-      </div>
+      {timeLeft > 0 && !showPopup && 
+      <p className="gameMessages">Time left...
+      <br />
+      <span className="seconds">{timeLeft} Sec</span></p>}
     </div>
   );
 };
